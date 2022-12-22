031e6f7167392c13d53659c3c5cb783fa8d53e02
정승현 <roach@woowahan.com>
2022-12-22 18:23:39 +0900

    꼬리 함수로 최적화

--- src/main/kotlin/math/Test.kt
+++ src/main/kotlin/math/Test.kt
@@ -1,25 +1,23 @@
 package math
 
-import kotlin.math.sqrt
+//private tailrec fun triangular(n: Int): Int = if (n == 0) 0 else n + triangular(n - 1)
 
-private fun triple(n: Int): Int = when {
-    n <= 0 -> 0
-    n == 1 -> 1
-    else -> n + triple(n - 1)
-}
+//private fun triangular2(n: Int): Int = (1 .. n).fold(0) { acc, i -> acc + i }
 
 private fun quad(n: Int): Int = n * n
 
 fun main() {
-    mutableListOf<Int>().also {
-        for (i in 0 .. 10000) {
-            it.add(triple(i))
-        }
-    }.run {
-        for (i in 0 .. 10000) {
-            if (this.contains(quad(i))) println(quad(i))
-        }
-    }
+//    mutableListOf<Int>().also {
+//        for (i in 0 .. 10000) {
+//            it.add(triangular(i))
+//        }
+//    }.run {
+//        for (i in 0 .. 10000) {
+//            if (this.contains(quad(i))) println(quad(i))
+//        }
+//    }
 
-    println(sqrt(41616.0))
+    println(triangular(100_000_000))
 }
+
+private tailrec fun triangular(n: Int, acc: Int = 0): Int = if (n == 0) acc else triangular(n - 1, acc + n)
