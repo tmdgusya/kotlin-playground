8e27c1c85b8b1de38b1953e8c9ccf5b63431005a
정승현 <roach@woowahan.com>
2023-01-24 19:44:48 +0900

    [Java to Kotlin book] currying test

---
+++ src/main/kotlin/interface/Currying.kt
@@ -0,0 +1,43 @@
+package `interface`
+
+import java.lang.IllegalArgumentException
+
+fun applyAWithB(f: (a: Int, b:Int) -> Int): (Int) -> (Int) -> Int {
+    fun applyA(a: Int): (Int) -> Int {
+        fun applyB(b: Int): Int {
+            return f(a, b)
+        }
+
+        return ::applyB
+    }
+
+    return ::applyA
+}
+
+fun <T : Any, R : Any> curriedLet(obj: T): ((T) -> R) -> R {
+    fun executeBlockWithObj(block: (T) -> R): R {
+        return block(obj)
+    }
+
+    return ::executeBlockWithObj
+}
+
+fun main() {
+    val sum = { a: Int, b: Int -> a + b}
+    val mult = {a: Int, b: Int -> a / b}
+    applyAWithB(sum)(1)(2)
+    applyAWithB(mult)(1)(2)
+    val curriedFunc = applyAWithB(sum)
+    val partialFunc = curriedFunc(1)
+    val funcUsePartial = partialFunc(2)
+    val funcUseOnlyCurrying = curriedFunc(1)(2)
+
+    var a: Int? = 1
+    val sumWithA = a?.let(curriedFunc) ?: throw IllegalArgumentException()
+    val result = sumWithA(2)
+    println(result)
+    println(result)
+
+    val applyBlockWithOne = curriedLet<Int, (Int) -> Int>(1)
+    applyBlockWithOne(curriedFunc)
+}
